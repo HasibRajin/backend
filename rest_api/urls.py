81e44fb528936be8859736dcff830dd71dc1eb2c
HasibRajin <thisisrajin@gmail.com>
2021-06-28 21:11:08 +0600

    add viewset

---
+++ rest_api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from rest_api import views
+
+
+router = DefaultRouter()
+
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+
+
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
+]
